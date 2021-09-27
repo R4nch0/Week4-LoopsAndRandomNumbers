1f292227fa2d77372fcaafce6d1480f7ec0b2a50
Kevin <kkivikas@tktk.com>
2021-09-27 15:50:48 +0300

    LoopsAndRandomNumbers

---
+++ GuessingGameThreeTries/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace GuessingGameThreeTries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* programm genereerib juhuslikku numbrit 1 kuni 10;
+              * kasutaja peab seda numbrit ära arvama;
+              * kui kasutaja suutis seda numbrit ära arvata, siis ta on mängu võitnud;
+              * kasutajal on 3 katset, kui kasutaja ei ole suutnud kolme katsega numbrit ära arvata
+              * siis mängu võidab arvuti.
+              * programm genereerib juhuslikku numbrit ühte korda
+              */
+
+            Random rnd = new Random();
+            int cpuNumber = rnd.Next(1, 11);
+            int i = 0;
+            
+            Console.WriteLine("Try to guess the number!");
+            while (i < 3)
+            {
+                Console.Write("Guess the Number: ");
+                int playerNumber = Convert.ToInt32(Console.ReadLine());
+                if (cpuNumber == playerNumber)
+                {
+                    Console.WriteLine("Right answer!");
+                    break;
+                }
+                else
+                {
+                    i++;
+                    if (i < 3)
+                    {
+                        Console.WriteLine("Guess again");
+                    }
+                }
+               
+            }
+            if (i == 3)
+            {
+                Console.WriteLine("You lose");
+            }
+        }
+
+    }
+}
---
+++ LoopsAndRandomNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LoopsAndRandomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* programm küsib kasutajal sisestada PIN-koodi;
+             * programm võrdleb sisestatud PIN-koodi arvuga 1234;
+             * kui sisestatud PIN-kood on 1234, siis konsool kuvab "Tere tulemast!";
+             * kui siesestatud PIN-kood on miadgi muud, siis konsool kuvab "Vale PIN. Proovi uuesti";
+             * kasutajal on kolm katset.  i < 3  >
+             * */
+
+            int i = 0;  //i- iteration
+
+            while (i < 3) //while-loop
+            {
+                Console.WriteLine("Siesesta PIN: ");
+                int userPIN = Convert.ToInt32(Console.ReadLine());              
+                
+                if(userPIN == 1234)
+                {
+                    Console.WriteLine("Tere tulemast!");
+                    break;
+                }
+                else
+                {
+                    i++;
+                    Console.WriteLine($"Vale Pin. {3 - i} katset on jäänud.");
+                }
+             
+            }
+
+                                
+            Console.WriteLine("Kena päeva!");
+
+        }
+    }
+}
